90e77cf8cbca3ae3a3fb0a1a8f51972423d898a9
Sumit <grgsten@gmail.com>
2024-07-30 10:05:34 +1000

    config removed from git ignore

---
+++ Config/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA9tIhtU-l1EpoUCR0x5YIA6v5s1M99Sn0",
+  authDomain: "artgalleryreactnative.firebaseapp.com",
+  projectId: "artgalleryreactnative",
+  storageBucket: "artgalleryreactnative.appspot.com",
+  messagingSenderId: "204026887502",
+  appId: "1:204026887502:web:6e35bfa33ab7dc34d0b21f",
+};
